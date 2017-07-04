7652d5fe836615aeb0f126e7f75537c9726b1ac5
rajmytaj <chalasanidheeraj@gmail.com>
2017-07-04 01:32:50 +0000

    function gets data and parses it into json object

--- download_avatars.js
+++ download_avatars.js
@@ -2,7 +2,34 @@ var request = require('request');
 
 console.log('welcome to the github avatar downloader');
 
+var GITHUB_USER = "";
+var GITHUB_TOKEN = "";
+
+function getRepoContributors(repoOwner, repoName, cb) {
+  var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
+  
+  var options = {
+  url: requestURL,
+  headers: {
+    'User-Agent': 'blah blah'
+  }
+};
+
+  request(options, function (error, response, body){
+    try {
+      const data = JSON.parse(body);
+      for (const prop in data){
+        console.log(prop);
+      }
+    } catch (err) {
+      console.log('Failed to parse content body');
+    }
+  })
+}
+
+
 getRepoContributors("jquery", "jquery", function(err, result) {
   console.log("Errors:", err);
   console.log("Result:", result);
 });
+// console.log(requestURL);
