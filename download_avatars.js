2c0eb738fa493131e00d73ca2a9a651fdc09947e
rajmytaj <chalasanidheeraj@gmail.com>
2017-07-04 00:06:31 +0000

    added getRepoContributors fcn

--- download_avatars.js
+++ download_avatars.js
@@ -1,3 +1,8 @@
 var request = require('request');
 
 console.log('welcome to the github avatar downloader');
+
+getRepoContributors("jquery", "jquery", function(err, result) {
+  console.log("Errors:", err);
+  console.log("Result:", result);
+});
